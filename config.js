a261bc90ca2eec6a967fef806435720b7243b8dd
Denethsds100 <65106755+Denethsds100@users.noreply.github.com>
2025-09-05 18:02:54 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// This file contains all the settings for your AR project.
+// Simply edit the values below.
+
+// The base URL for your GitHub repository.
+// To get this URL, go to your repository, click on any file, and then click the "Raw" button.
+// Copy the part of the URL that comes before the file name.
+export const githubRepoBaseUrl = "https://raw.githubusercontent.com/Denethsds100/Denethsds-AR/main/";
+
+// The name of your 3D model file.
+export const modelFileName = "ARmodel";
+
+// The name of your image target file (in .gltf format).
+export const imageTargetFileName = "image_target.gltf";
